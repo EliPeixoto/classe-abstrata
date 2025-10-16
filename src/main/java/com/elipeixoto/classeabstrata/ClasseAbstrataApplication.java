44dc1338642ab8b81d0fc1829aa490366f75f82e
EliPeixoto <eliane.peixoto@outlook.com>
2025-10-16 07:25:55 -0300

    first commit

---
+++ src/main/java/com/elipeixoto/classeabstrata/ClasseAbstrataApplication.java
@@ -0,0 +1,13 @@
+package com.elipeixoto.classeabstrata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasseAbstrataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClasseAbstrataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elipeixoto/classeabstrata/ClasseAbstrataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elipeixoto.classeabstrata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasseAbstrataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
